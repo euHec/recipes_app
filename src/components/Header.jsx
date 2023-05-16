e2f42f3560b1ef128108982b537a64631546f12f
euHec <hectorsouzass@gmail.com>
2023-05-16 19:59:58 -0300

    style: Implementa estilização do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,48 +1,39 @@
-import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { useContext } from 'react';
+import { BsSearch, BsPerson } from 'react-icons/bs';
 import profileIconSvg from '../images/profileIcon.svg';
 import searchIconSvg from '../images/searchIcon.svg';
 import { RecipesContext } from '../context/RecipesContext';
+import logo from '../images/logo Recipes App.svg';
 
-export default function Header({ pageTitle, showSearchIcon = false }) {
+export default function Header() {
   const { setToggleShowSearch } = useContext(RecipesContext);
   const history = useHistory();
   return (
-    <header className="w-full flex justify-around">
+    <header
+      className="w-full h-28 flex
+      justify-around border-solid
+      border-b border-black fixed bg-cyan-600 top-0 left-0"
+    >
       <button
         data-testid="profile-top-btn"
         onClick={ () => { history.push('/profile'); } }
         src={ profileIconSvg }
+        className="bg-transparent w-24 flex justify-center items-center"
       >
-        <img
-          src={ profileIconSvg }
-          alt="icone de perfil"
-        />
+        <BsPerson className="text-5xl text-cyan-50" />
       </button>
-
-      <h2 data-testid="page-title">{ pageTitle }</h2>
-      {
-        showSearchIcon
-          && (
-            <button
-              data-testid="search-top-btn"
-              src={ searchIconSvg }
-              onClick={ () => { setToggleShowSearch((prevState) => !prevState); } }
-            >
-              <img
-                src={ searchIconSvg }
-                alt="icone de busca"
-              />
-            </button>
-
-          )
-      }
+      <div className="flex w-24">
+        <img className="w-full" src={ logo } alt="logo app" />
+      </div>
+      <button
+        data-testid="search-top-btn"
+        src={ searchIconSvg }
+        onClick={ () => { setToggleShowSearch((prevState) => !prevState); } }
+        className="bg-transparent w-24 flex justify-center items-center"
+      >
+        <BsSearch className="text-4xl text-cyan-50" />
+      </button>
     </header>
   );
 }
-
-Header.propTypes = {
-  pageTitle: PropTypes.string.isRequired,
-  showSearchIcon: PropTypes.bool,
-};
